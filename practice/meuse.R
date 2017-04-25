12965d4464cc0e8c5184ef337f213c3e20210c37
tonyxuantong <xuantong@sas.upenn.edu>
2017-04-24 23:33:07 -0600

    Week4

---
+++ practice/meuse.R
@@ -0,0 +1,13 @@
+## Example using built-in dataset from the sp package
+library(sp)
+## Load required datastes
+data(meuse)
+# The meuse dataset is not by default a spatial object
+# but its x, y coordinates are part of the data.frame
+class(meuse)
+## [1] "data.frame"
+coordinates(meuse) <- c("x", "y")
+class(meuse)
+## [1] "SpatialPointsDataFrame"
+## attr(,"package")
+## [1] "sp"
